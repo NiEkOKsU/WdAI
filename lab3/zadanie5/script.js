d3f7d1b23320565bdeb39113842c6cc46713f3ef
NiEkOKsU <bartek.kozera00@gmail.com>
2022-11-09 13:00:59 +0100

    backup

--- lab3/zadanie5/script.js
+++ lab3/zadanie5/script.js
@@ -1,129 +1,148 @@
-let points = 0;
-let clicked = false;
-let propagation = true;
+let points = 0
+let clicked = false
+let propagation = true
 
-let blue_clicked = false;
-let red_clicked = false;
-let yellow_clicked = false;
+let howMuchPoint = [1, 2, 5]
+const boxNum = 3
+let number = 0
 
-function changePoints(addd){
-    points += addd;
-    document.getElementById("points").textContent = points;
+let small_clicked = false
+let med_clicked = false
+let big_clicked = false
+let smallOver = false
+let medOver = false
+
+let small = document.getElementById("small")
+let med = document.getElementById("med")
+let big = document.getElementById("big")
+let reset = document.getElementById("reset")
+let propagationButton = document.getElementById("propagation")
+let change = document.getElementById("change")
+
+let el
+let textNode
+
+function addToList(){
+    if(small_clicked && !smallOver){
+        el = document.createElement("li");
+        console.log(el)
+        textNode = document.createTextNode("Dodano " + howMuchPoint[(number + 2) % boxNum] + " pkt");
+        el.appendChild(textNode);
+        document.getElementById("list").appendChild(el);
+    }
+    if(med_clicked && !medOver){
+        el = document.createElement("li");
+        textNode = document.createTextNode("Dodano " + howMuchPoint[(number + 1) % boxNum] + " pkt");
+        el.appendChild(textNode);
+        document.getElementById("list").appendChild(el);
+    }
+    if(big_clicked){
+        el = document.createElement("li");
+        textNode = document.createTextNode("Dodano " + howMuchPoint[(number) % boxNum] + " pkt");
+        el.appendChild(textNode);
+        document.getElementById("list").appendChild(el);
+    }
+}
+function addToSmall(){
+    points += howMuchPoint[(number + 2) % boxNum]
+    small_clicked = true
+    document.getElementById("points").textContent = points
 }
 
-function resetbuttons(){
-    blue_clicked = false;
-    red_clicked = false;
-    yellow_clicked = false;
-}
-
-function clearInfo(){
-    document.getElementById("info1").textContent = "-";
-    document.getElementById("info2").textContent = "-";
-    document.getElementById("info3").textContent = "-";
-}
-
-function displayInfo(){
-    document.getElementById("info1").textContent= "-"
-    document.getElementById("info2").textContent= "-"
-    document.getElementById("info3").textContent= "-"
-    if (blue_clicked)
-        document.getElementById("info1").textContent= "Nacisnąłeś niebieski o wartości 1"
-    if (red_clicked)
-        document.getElementById("info2").textContent= "Nacisnąłeś czerwony o wartości 2"
-    if (yellow_clicked)
-        document.getElementById("info3").textContent= "Nacisnąłeś żółty o wartości 5"
-}
-
-function yellowClick(){
-    if (points>50)
-        return;
-    changePoints(5);
-    yellow_clicked = true;
-    if (!propagation){
-        displayInfo();
-        resetbuttons();
+function addToMed(){
+    if (!small_clicked || propagation){
+        points += howMuchPoint[(number + 1) % boxNum]
+        med_clicked = true
+        document.getElementById("points").textContent = points
     }
 }
 
-
-function redClick(){
-    if (points>30)
-        return;
-    red_clicked = true;
-    if (!propagation){
-        displayInfo();
-        resetbuttons();
+function addToBig(){
+    if ((!small_clicked && !med_clicked) || propagation){
+        points += howMuchPoint[number % boxNum]
+        document.getElementById("points").textContent = points
+        big_clicked = true
     }
-        
-    changePoints(2);
+    addToList()
+    pointStatusCheck()
 }
 
-function blueClick(){
-    changePoints(1);
-    blue_clicked = true;
-    displayInfo();
-    resetbuttons();
+function setSmallTrue(){
+    small_clicked = true
 }
 
-function clickHandler(id){
-    if(!clicked){
-        switch(id){
-            case "big":
-                blueClick();
-                break;
-            case "med":
-                redClick();
-                break;
-            case "small":
-                yellowClick();
-                break;
-        }
+function setMedTrue(){
+    med_clicked = true
+}
 
+function pointStatusCheck(){
+    if (points > 30){
+        med.removeEventListener('click', addToMed, false)
+        med.addEventListener('click', setMedTrue, false)
+        medOver = true
     }
-    if (!propagation)
-        clicked = true;
+    if (points > 50){
+        small.removeEventListener('click', addToSmall, false)
+        small.addEventListener('click', setSmallTrue, false)
+        smallOver = true
+    }
+    small_clicked = false
+    med_clicked = false
+    big_clicked = false
 }
 
-function clickHandlerReset(){
-    clicked = false;
-}
-
-
-function reset(){
-    points = 0;
-    clearInfo();
-    resetbuttons();
-    document.getElementById("points").textContent = points;
+function restart(){
+    points = 0
+    number = 0
+    document.getElementById("points").textContent = points
+    let toRemove = document.getElementById("list");
+    len = toRemove.childNodes.length
+    for (i = 0; i < len; i++){
+        toRemove.removeChild(toRemove.childNodes[0]);
+    }
+    el = document.createElement("li");
+    textNode = document.createTextNode("Zresetowano");
+    el.appendChild(textNode);
+    document.getElementById("list").appendChild(el);
+    start()
 }
 
 function prop(){
-    if (this.classList.contains("cur-stop")){
-        propagation = false;
-        this.classList.remove("cur-stop");
-        this.classList.add("cur-start");
-        this.textContent="Start Propagation";
+    if(propagation){
+        propagation = false
+        propagationButton.innerHTML = "Start propagation"
+        el = document.createElement("li");
+        textNode = document.createTextNode("Propagacja zatrzymana");
+        el.appendChild(textNode);
+        document.getElementById("list").appendChild(el);
     }
-    else if (this.classList.contains("cur-start")){
-        propagation = true;
-        this.classList.remove("cur-start");
-        this.classList.add("cur-stop");
-        this.textContent="Stop Propagation";
+    else{
+        propagation = true
+        propagationButton.innerHTML = "Stop propagation"
+        el = document.createElement("li");
+        textNode = document.createTextNode("Propagacja wznowiona");
+        el.appendChild(textNode);
+        document.getElementById("list").appendChild(el);
     }
-    return
 }
 
+function changer(){
+    number += 1
+    el = document.createElement("li");
+    textNode = document.createTextNode("Zmieniono wartościowanie pudełek");
+    el.appendChild(textNode);
+    document.getElementById("list").appendChild(el);
+}
 
-document.getElementById("reset").addEventListener('click', reset);
-document.getElementById("propagation").addEventListener('click', prop);
-document.getElementById("small").addEventListener('click', () => {
-    clickHandler("small");
-});
-document.getElementById("med").addEventListener('click', () => {
-    clickHandler("med");
-});
-document.getElementById("big").addEventListener('click', () => {
-    clickHandler("big");
-    clickHandlerReset();
-});
-document.getElementById("points").textContent = points;
+function start() {
+    if (points === 0){
+        small.addEventListener('click', addToSmall, false)
+        med.addEventListener('click', addToMed, false)
+        big.addEventListener('click', addToBig, false)
+        reset.addEventListener('click', restart, false)
+        propagationButton.addEventListener('click', prop, false)
+        change.addEventListener('click', changer, false)
+    }
+}
+
+start()
